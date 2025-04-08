51ba96466efa6a5785d1f9aa4fa0e2cfb1622287
Matheus Almeida <matheus.almeida.dev2024@gmail.com>
2025-04-08 15:42:06 -0300

    updated routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ import { registerUser, loginUser } from "../services/authService.js";
 export const register = async (req, res, next) => {
   try {
     const user = await registerUser(req.body);
-    res.status(201).json({ message: "Usuário registrado com sucesso", user });
+    res.status(201).json({ message: "User registered succesfully", user });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- src/services/authService.js
+++ src/services/authService.js
@@ -6,7 +6,7 @@ const SECRET = process.env.JWT_SECRET || "secreto_dev";
 
 export const registerUser = async (data) => {
   const existingUser = await User.findOne({ email: data.email });
-  if (existingUser) throw new Error("Email já cadastrado");
+  if (existingUser) throw new Error("Email already registered");
 
   const hashedPassword = await bcrypt.hash(data.password, 10);
   const newUser = new User({
@@ -21,10 +21,10 @@ export const registerUser = async (data) => {
 
 export const loginUser = async ({ email, password }) => {
   const user = await User.findOne({ email });
-  if (!user) throw new Error("Credenciais inválidas");
+  if (!user) throw new Error("Invalid credentials");
 
   const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) throw new Error("Credenciais inválidas");
+  if (!isMatch) throw new Error("Invalid credentials");
 
   const token = jwt.sign({ id: user._id, role: user.role }, SECRET, {
     expiresIn: "1d",
